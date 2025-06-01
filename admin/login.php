86a70f1bd6d5f9177d515a1b9b98c050d492537f
TeaLance <lanceleegithub@gmail.com>
2025-06-02 03:51:44 +0800

    admin頁面，支援輸入正確帳密後跳轉至index.html

---
+++ admin/login.php
@@ -0,0 +1,19 @@
+<?php
+$env = parse_ini_file(__DIR__ . '/passwd.env');
+$valid_username = $env['USERNAME'];
+$valid_password = $env['PASSWORD'];
+
+$input_username = $_POST['username'] ?? '';
+$input_password = $_POST['password'] ?? '';
+
+if ($input_username === $valid_username && $input_password === $valid_password) {
+    header("Location: ../index.html");
+    exit();
+
+} else {
+    //  登入失敗
+    $error_msg = urlencode("帳號或密碼錯誤，請重新輸入！");
+    header("Location: login.html?error=$error_msg");
+    exit();
+}
+?>
